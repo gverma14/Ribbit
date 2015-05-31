23876226b8515b35b1bd5c32df4e5b660f087529
Gaurav Verma <gv1031@gmail.com>
2015-05-31 17:41:10 -0400

    Initial Commit

---
+++ Ribbit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Ribbit
+//
+//  Created by Gaurav Verma on 5/31/15.
+//  Copyright (c) 2015 Shiny Mango. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
