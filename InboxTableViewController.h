551b6127ce46dd97ce074fe1ac8dd9f95259f5da
Gaurav Verma <gv1031@gmail.com>
2015-06-01 12:28:26 -0400

    Added log in and sign up screens to story board with auto layout constraints. Created InboxViewController object files.

---
+++ InboxTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  InboxTableViewController.h
+//  Ribbit
+//
+//  Created by Gaurav Verma on 6/1/15.
+//  Copyright (c) 2015 Shiny Mango. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface InboxTableViewController : UITableViewController
+
+@end
