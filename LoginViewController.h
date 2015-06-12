291ba44aa32d507d5ced3e889861e625108d64c3
Gaurav Verma <gv1031@gmail.com>
2015-06-12 11:15:24 -0400

    Created login and sign up view controllers and linked to storyboard

---
+++ LoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.h
+//  Ribbit
+//
+//  Created by Gaurav Verma on 6/12/15.
+//  Copyright (c) 2015 Shiny Mango. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginViewController : UIViewController
+
+@end
---
+++ Ribbit/SignUpViewController.h
@@ -0,0 +1,13 @@
+//
+//  SignUpViewController.h
+//  Ribbit
+//
+//  Created by Gaurav Verma on 6/12/15.
+//  Copyright (c) 2015 Shiny Mango. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SignUpViewController : UIViewController
+
+@end
