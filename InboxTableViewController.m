52fad2434b78130880b3a187ef47130068a20cc3
Gaurav Verma <gv1031@gmail.com>
2015-06-12 11:03:39 -0400

    Removed test object code for Parse.com

--- InboxTableViewController.m
+++ InboxTableViewController.m
@@ -7,7 +7,7 @@
 //
 
 #import "InboxTableViewController.h"
-
+#import <Parse/Parse.h>
 @interface InboxTableViewController ()
 
 @end
@@ -17,11 +17,16 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
+    
+    
+    
     // Uncomment the following line to preserve selection between presentations.
     // self.clearsSelectionOnViewWillAppear = NO;
     
     // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
     // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+    
+    
     [self performSegueWithIdentifier:@"showLogin" sender:self];
 }
 
