c9a190ee53b55a5aad31a737e05bc3accdaa7fc5
Gaurav Verma <gv1031@gmail.com>
2015-06-13 00:00:04 -0400

    Create FriendsViewController and link with storyboard

---
+++ Ribbit/FriendsViewController.h
@@ -0,0 +1,13 @@
+//
+//  FriendsViewController.h
+//  Ribbit
+//
+//  Created by Gaurav Verma on 6/12/15.
+//  Copyright (c) 2015 Shiny Mango. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <Parse/Parse.h>
+@interface FriendsViewController : UITableViewController
+
+@end
---
+++ Ribbit/FriendsViewController.m
@@ -0,0 +1,37 @@
+//
+//  FriendsViewController.m
+//  Ribbit
+//
+//  Created by Gaurav Verma on 6/12/15.
+//  Copyright (c) 2015 Shiny Mango. All rights reserved.
+//
+
+#import "FriendsViewController.h"
+
+@interface FriendsViewController ()
+
+@end
+
+@implementation FriendsViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+ 
+}
+
+
+#pragma mark - Table view data source
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+#warning Potentially incomplete method implementation.
+    // Return the number of sections.
+    return 0;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+#warning Incomplete method implementation.
+    // Return the number of rows in the section.
+    return 0;
+}
+
+@end
