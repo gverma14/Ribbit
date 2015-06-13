908817957a2b97adef9dd5b6990002670c8c3ec6
Gaurav Verma <gv1031@gmail.com>
2015-06-12 22:43:40 -0400

    Created the EditFriendsViewController and added storyboard components

---
+++ Ribbit/EditFriendsViewController.m
@@ -0,0 +1,47 @@
+//
+//  EditFriendsViewController.m
+//  Ribbit
+//
+//  Created by Gaurav Verma on 6/12/15.
+//  Copyright (c) 2015 Shiny Mango. All rights reserved.
+//
+
+#import "EditFriendsViewController.h"
+#import <Parse/Parse.h>
+@interface EditFriendsViewController ()
+
+@end
+
+@implementation EditFriendsViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    PFQuery *query = [PFUser query];
+    [query orderByAscending:@"username"];
+    [query findObjectsInBackgroundWithBlock:^(NSArray *objects, NSError *error) {
+        if (error) {
+            NSLog(@"Error: %@ %@", error, [error userInfo]);
+        }
+        else {
+            
+        }
+    }]
+}
+
+
+#pragma mark - Table view data source
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    // Return the number of sections.
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+#warning Incomplete method implementation.
+    // Return the number of rows in the section.
+    return 0;
+}
+
+
+
+@end
