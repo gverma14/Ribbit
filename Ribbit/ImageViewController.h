04c1da99623a6ef660d273d186c04335a9dbe75e
Gaurav Verma <gv1031@gmail.com>
2015-06-14 23:20:26 -0400

    Create ImageViewController for opening image messages

---
+++ Ribbit/ImageViewController.h
@@ -0,0 +1,13 @@
+//
+//  ImageViewController.h
+//  Ribbit
+//
+//  Created by Gaurav Verma on 6/14/15.
+//  Copyright (c) 2015 Shiny Mango. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ImageViewController : UIViewController
+
+@end
